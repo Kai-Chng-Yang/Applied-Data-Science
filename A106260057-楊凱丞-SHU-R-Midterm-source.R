411b99f1e0a409149a63c1e653047ad885c540bc
Kai-Chng-Yang <71475667+Kai-Chng-Yang@users.noreply.github.com>
2020-12-11 12:02:57 +0800

    Add files via upload

---
+++ A106260057-楊凱丞-SHU-R-Midterm-source.R
@@ -0,0 +1,33 @@
+# 2020/12/11(??), 109?Ǧ~?Ĥ@?Ǵ? ???Ƭ???��?? R?��???
+#
+# ?Ǹ?:        ?m?W: 
+#
+# ???ɮ׬??U?D???{???X?ɡA?L???浲?G
+
+
+# ex1(a)
+study <- function(x){
+  Tuition <- 400*x + 600*y
+  ifelse( Tution > 12000, "*") 
+}
+
+# ex2(a)
+library(readxl)
+Score <- read_excel("1091-SHU-R-Midterm/Score-109.xlsx", skip = 1)
+head(Score, 5)
+tail(Score, 5)
+
+# ex2(b)
+
+# ex2(C)
+my.cor <- function(x){
+  
+}
+
+
+# ex2(d)
+
+# ex3(a)
+my.dnorm <- function(x){
+  
+}
